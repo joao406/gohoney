a7956f9d302a083032f91e3799cb5eff55800acb
João <145498965+joao406@users.noreply.github.com>
2024-02-01 03:28:36 +0000

    Add files via upload

---
+++ systems/genrelatory.go
@@ -0,0 +1,51 @@
+package systems
+
+import (
+	"html/template"
+	"io/ioutil"
+	"log"
+)
+
+type relatoryPage {
+	Time      string
+	IPAddr    string
+	UserAgent string
+}
+
+func GenerateRelatory(r *http.Request) (*relatoryPage, error) {
+	relatory := &relatoryPage{
+		Time:      time.Now().Format("2006-01-02 15:04:05"),
+		IPAddr:    r.RemoteAddr,
+		UserAgent: r.Header.Get("User-Agent"),
+	}
+
+	fileName := fmt.Sprintf("relatory_%s.html", time.Now().Format("2006-01-02"))	
+
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	
+	tmpl := template.Must(template.New("relatorypage").Parse(`
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Relatório</title>
+</head>
+<body>
+    <h1>Relatório</h1>
+    <p>Data e Hora: {{.Time}}</p>
+    <p>Endereço IP: {{.IPAddr}}</p>
+    <p>User-Agent: {{.UserAgent}}</p>
+</body>
+</html>
+`))
+
+	err = tmpl.Execute(file, relatory)
+	if err != nil {
+		return nil, err
+	}
+
+	return relatory, nil
+}
